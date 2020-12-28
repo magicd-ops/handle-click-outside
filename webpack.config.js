f83c99791e840f25b82d052f855ea723c5e27dd3
Magicd <magictest2020@gmail.com>
2020-12-29 01:53:27 +0330

    add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
-//const path = require("path");
-//const HtmlWebpackPlugin = require("html-webpack-plugin");
-//const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
 module.exports = {
   output: {
     filename: 'src/index.js'
+  },
+  module: {
+    rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }
+    ]
   }
 };
